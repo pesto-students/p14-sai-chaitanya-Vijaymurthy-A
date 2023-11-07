e16cfc07a532df237bbb94c3357b77da5cddb796
Vijaymurthy-A <vijaymurthy95.a@gmail.com>
2023-11-07 18:28:19 +0530

    Fetch API assignment using async/await is added

---
+++ Week-5/Session-12_async_js/asyncAwait.js
@@ -0,0 +1,34 @@
+async function getExchangeRate(currency) {
+  try {
+    var myHeaders = new Headers();
+    myHeaders.append("apiKey", "wfZ0DhsWufJUiw9OmBJLxtaLeLQvQZw9");
+    var requestOptions = {
+      headers: myHeaders,
+    };
+    const responseData = await fetch(
+      `https://api.apilayer.com/currency_data/convert?to=EUR&from=${currency}&amount=1`,
+      requestOptions
+    );
+    if (responseData.ok) {
+      const responseJson = await responseData.json();
+
+      return Object.hasOwn(responseJson, "result")
+        ? responseJson.result.toFixed(4)
+        : null;
+    } else {
+      console.log("Failed Fetching data from the API");
+      return null;
+    }
+  } catch (err) {
+    console.log(`Exception Occured : ${err}`);
+    throw err;
+  }
+}
+
+getExchangeRate("KWD")
+  .then((res) => {
+    console.log(`Exchange Conversion rate to EUR is ${res}`);
+  })
+  .catch((err) => {
+    console.error(`Exception Occured : ${err}`);
+  });
