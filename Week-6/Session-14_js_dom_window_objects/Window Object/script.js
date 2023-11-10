428c2fafe7fd30a780df060c12774475a574e527
Vijaymurthy-A <vijaymurthy95.a@gmail.com>
2023-11-11 00:30:01 +0530

    JS Advanced assignments are added

---
+++ Week-6/Session-14_js_dom_window_objects/Window Object/script.js
@@ -0,0 +1,38 @@
+function displayWindowProperties() {
+  console.log(`Navigator : ${navigator.userAgent}`);
+  console.log(`Screen Width : ${screen.width} and height : ${screen.height}`);
+  console.log(
+    `Location href : ${location.href} and path name : ${location.pathname}`
+  );
+
+  //   Working with Local storage
+  localStorage.setItem("name", "Vijay");
+  localStorage.setItem("batch", "p-14");
+  console.log(localStorage.getItem("name"));
+  console.log(localStorage.getItem("batch"));
+  console.log("Length of local storage is  : " + localStorage.length);
+  localStorage.removeItem("batch");
+  console.log("Length after removing one item  : " + localStorage.length);
+
+  //   Working with Session storage
+  sessionStorage.setItem("name", "Vijay");
+  sessionStorage.setItem("batch", "p-14");
+  console.log(sessionStorage.getItem("name"));
+  console.log(sessionStorage.getItem("batch"));
+  console.log("Length of local storage is  : " + sessionStorage.length);
+  sessionStorage.removeItem("batch");
+  console.log("Length after removing one item  : " + sessionStorage.length);
+
+  //   Set cookie with expiration of 7 days
+  let expDate = new Date();
+  expDate.setDate(expDate.getDate() + 7);
+  document.cookie = "name=vijay;expires=" + expDate.toUTCString();
+  console.log(document.cookie);
+
+  //   Remove cookie
+  setTimeout(() => {
+    console.log("Removing Cookie");
+    expDate.setDate(expDate.getDate() - 10);
+    document.cookie = "name=;expires" + expDate.toUTCString();
+  }, 10000);
+}
---
+++ Week-6/Session-15_JS_Behind_Scenes_JS_Engine/ES6 Modules/index.js
@@ -0,0 +1,5 @@
+import { add } from "./calculator.js";
+import subtract from "./calculator.js";
+
+console.log("Addition of 5 & 5 is :" + add(5, 5));
+console.log("Subtraction of 50 & 5 is :" + subtract(50, 5));
